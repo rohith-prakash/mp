2b4779488d1474463b2e2ed0cba53f53425c09fc
rohith-prakash <rohithprakashklm@gmail.com>
2021-08-13 11:42:59 +0530

    converting to string  completed

--- bigint/bigint.go
+++ bigint/bigint.go
@@ -1,1 +1,61 @@
 package bigint
+
+import (
+	"errors"
+	"regexp"
+	"strings"
+)
+
+type Sign int
+
+const (
+	Positive Sign = iota
+	Negative
+)
+
+type BigInt struct {
+	sign Sign
+	num  []uint8
+}
+
+func (a *BigInt) addToNum(c rune) {
+	a.num = append(a.num, uint8(c-'0'))
+}
+
+func StrToBigInt(number string) (BigInt, error) {
+	number = strings.ReplaceAll(number, " ", "")
+	matched, err := regexp.MatchString(`[-+]?[0-9]`, number)
+	if err != nil {
+		return BigInt{}, err
+	} else if !matched {
+		return BigInt{}, errors.New("Not a valid number to be converted to BigInt")
+	}
+	var num BigInt
+	num.num = make([]uint8, 0)
+	if number[0] == '-' || number[0] == '+' {
+		if number[0] == '-' {
+			num.sign = Negative
+		} else if number[0] == '+' {
+			num.sign = Positive
+		}
+		for _, c := range number[1:] {
+			num.addToNum(c)
+		}
+	} else {
+		for _, c := range number {
+			num.addToNum(c)
+		}
+	}
+	return num, nil
+}
+
+func (num BigInt) ToString() string {
+	var sb strings.Builder
+	if num.sign == Negative {
+		sb.WriteString("-")
+	}
+	for i := range num.num {
+		sb.WriteString(string(num.num[i] + '0'))
+	}
+	return sb.String()
+}
--- main.go
+++ main.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"github.com/rohith-prakash/mp/bigint"
+	_ "github.com/rohith-prakash/mp/bigint"
+)
+
+func main() {
+	a := "12345"
+	b := "- 12345"
+	c, err := bigint.StrToBigInt(a)
+	if err != nil {
+		fmt.Println(err)
+	}
+	d, err := bigint.StrToBigInt(b)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(c)
+	fmt.Println(d)
+	fmt.Println(c.ToString())
+	fmt.Println(d.ToString())
 }
