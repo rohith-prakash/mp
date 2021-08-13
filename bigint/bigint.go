8b7460a693010032814e9337c1e7e01d598b0c33
rohith-prakash <rohithprakashklm@gmail.com>
2021-08-13 14:48:04 +0530

    rearranged the ordering

--- bigint/bigint.go
+++ bigint/bigint.go
@@ -12,15 +12,6 @@ const (
 	Negative
 )
 
-type Ordering int
-
-const (
-	Lesser Ordering = iota
-	Equal
-	Greater
-	NotEqual
-)
-
 type BigInt struct {
 	sign Sign
 	num  []uint8
@@ -94,59 +85,4 @@ func (num BigInt) ToString() string {
 		sb.WriteString(string(num.num[i] + '0'))
 	}
 	return sb.String()
-}
-
-func MagnitudeCompare(A BigInt, B BigInt) Ordering {
-	a := A.num
-	b := B.num
-	a_len := len(a)
-	b_len := len(b)
-	if a_len > b_len {
-		return Greater
-	} else if a_len < b_len {
-		return Lesser
-	} else {
-		for i := 0; i < a_len; i++ {
-			if a[i] > b[i] {
-				return Greater
-			} else if a[i] < b[i] {
-				return Lesser
-			}
-		}
-		return Equal
-	}
-}
-
-func LogicalNegate(order Ordering) Ordering {
-	switch order {
-	case Lesser:
-		return Greater
-	case Greater:
-		return Lesser
-	case Equal:
-		return NotEqual
-	case NotEqual:
-		return Equal
-	default:
-		return NotEqual
-	}
-}
-
-func Compare(a BigInt, b BigInt) Ordering {
-	if a.sign == Positive && b.sign == Negative {
-		return Greater
-	} else if a.sign == Negative && b.sign == Positive {
-		return Lesser
-	} else {
-		var magnitudeCompared = MagnitudeCompare(a, b)
-		if a.sign == Positive {
-			return magnitudeCompared
-		} else {
-			if magnitudeCompared == Equal {
-				return magnitudeCompared
-			} else {
-				return LogicalNegate(magnitudeCompared)
-			}
-		}
-	}
 }
---
+++ bigint/ordering.go
@@ -0,0 +1,65 @@
+package bigint
+
+type Ordering int
+
+const (
+	Lesser Ordering = iota
+	Equal
+	Greater
+	NotEqual
+)
+
+func MagnitudeCompare(A BigInt, B BigInt) Ordering {
+	a := A.num
+	b := B.num
+	a_len := len(a)
+	b_len := len(b)
+	if a_len > b_len {
+		return Greater
+	} else if a_len < b_len {
+		return Lesser
+	} else {
+		for i := 0; i < a_len; i++ {
+			if a[i] > b[i] {
+				return Greater
+			} else if a[i] < b[i] {
+				return Lesser
+			}
+		}
+		return Equal
+	}
+}
+
+func LogicalNegate(order Ordering) Ordering {
+	switch order {
+	case Lesser:
+		return Greater
+	case Greater:
+		return Lesser
+	case Equal:
+		return NotEqual
+	case NotEqual:
+		return Equal
+	default:
+		return NotEqual
+	}
+}
+
+func Compare(a BigInt, b BigInt) Ordering {
+	if a.sign == Positive && b.sign == Negative {
+		return Greater
+	} else if a.sign == Negative && b.sign == Positive {
+		return Lesser
+	} else {
+		var magnitudeCompared = MagnitudeCompare(a, b)
+		if a.sign == Positive {
+			return magnitudeCompared
+		} else {
+			if magnitudeCompared == Equal {
+				return magnitudeCompared
+			} else {
+				return LogicalNegate(magnitudeCompared)
+			}
+		}
+	}
+}
